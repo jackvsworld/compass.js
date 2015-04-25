30e737f60678d78b558d1df44c51ad33fd1dc017
jackvsworld <ajbarton@users.sf.net>
2015-04-25 13:43:41 -0700

    default radius=1
    Make radius an optional parameter

--- src/compass.js
+++ src/compass.js
@@ -1,5 +1,6 @@
 function Compass(context, x, y, n) {
   this.draw = function (dx, dy, r) {
+    var r = (r || 1);
     context.beginPath();
     context.arc(n*(dx + x), n*(dy + y), n*r, 0, 2*Math.PI);
     context.stroke();
