d404ef6ba6102d186b7a39621a4f4e96915d0181
jackvsworld <ajbarton@users.sf.net>
2015-04-25 21:25:49 -0700

    colors - fixes #1
    Squashed commit of the following:
    
    commit 8deccd70d0f2e82480a09f348431a37c4f4df84b
    Author: jackvsworld <ajbarton@users.sf.net>
    Date:   Sat Apr 25 21:13:19 2015 -0700
    
        Enable colors on canvas
    
    commit 69646ba26cd2c6f6bd896456be4de8b1e7523917
    Author: jackvsworld <ajbarton@users.sf.net>
    Date:   Sat Apr 25 14:15:37 2015 -0700
    
        Add methods color() and fill()

--- src/compass.js
+++ src/compass.js
@@ -1,8 +1,26 @@
 function Compass(context, x, y, n) {
+  this.options = {
+    color: 'black',
+    fill: 'transparent'
+  };
+
+  this.color = function (color) {
+    this.options.color = color;
+    return this;
+  };
+
+  this.fill = function (color) {
+    this.options.fill = color;
+    return this;
+  };
+
   this.draw = function (dx, dy, r) {
     var r = (r || 1);
+    context.strokeStyle = this.options.color;
+    context.fillStyle = this.options.fill;
     context.beginPath();
     context.arc(n*(dx + x), n*(dy + y), n*r, 0, 2*Math.PI);
+    context.fill();
     context.stroke();
   };
 }
